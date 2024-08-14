ac87ac1516e400c0b671ccdf39c077f723f1873e
NotCoded <66999075+not-coded@users.noreply.github.com>
2024-08-14 22:30:35 +0200

    i love suppressings warnings

--- nexus-builder/src/main/java/com/nexia/nexus/builder/mixin/_bugfixes/PlayerMixin.java
+++ nexus-builder/src/main/java/com/nexia/nexus/builder/mixin/_bugfixes/PlayerMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Player.class)
-@SuppressWarnings("UnusedMethod")
+@SuppressWarnings({"UnusedMethod", "UnusedVariable"})
 public abstract class PlayerMixin extends LivingEntity {
 
     protected PlayerMixin(EntityType<? extends LivingEntity> entityType, Level level) {
