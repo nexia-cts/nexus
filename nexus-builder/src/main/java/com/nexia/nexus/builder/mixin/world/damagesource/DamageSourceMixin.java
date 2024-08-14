30eba13c9bbd62b05febc6d1b7f8190f031299e2
Infinityy <120841985+Inf1nityy@users.noreply.github.com>
2024-08-14 20:35:08 +0200

    fix shields not being able to block explosions

--- nexus-builder/src/main/java/com/nexia/nexus/builder/mixin/world/damagesource/DamageSourceMixin.java
+++ nexus-builder/src/main/java/com/nexia/nexus/builder/mixin/world/damagesource/DamageSourceMixin.java
@@ -5,7 +5,11 @@ import com.nexia.nexus.builder.extension.wrap.Wrap;
 import com.nexia.nexus.builder.implementation.util.ObjectMappings;
 import com.nexia.nexus.builder.implementation.world.damage.WrappedDamageData;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.EntityDamageSource;
+import net.minecraft.world.level.Explosion;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -29,5 +33,15 @@ public abstract class DamageSourceMixin implements Wrap<DamageData> {
     @Override
     public DamageData wrap() {
         return wrapped;
+    }
+
+    /**
+     * @author NotInfinityy
+     * @reason Fix shields not being able to block explosions due to getting the wrong source position
+     */
+    @Overwrite
+    public static DamageSource explosion(@Nullable Explosion explosion) {
+        assert explosion != null;
+        return new EntityDamageSource("explosion", explosion.source).setScalesWithDifficulty().setExplosion();
     }
 }
