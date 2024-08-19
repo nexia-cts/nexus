d46932e2dade10ddb3cae4aad42b07caf288132a
Infinityy <120841985+Inf1nityy@users.noreply.github.com>
2024-08-19 04:13:23 +0200

    fix shield break sound

--- nexus-builder/src/main/java/com/nexia/nexus/builder/mixin/_bugfixes/PlayerMixin.java
+++ nexus-builder/src/main/java/com/nexia/nexus/builder/mixin/_bugfixes/PlayerMixin.java
@@ -2,7 +2,6 @@ package com.nexia.nexus.builder.mixin._bugfixes;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.core.BlockPos;
-import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.EntityType;
@@ -11,7 +10,8 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Player.class)
 @SuppressWarnings({"UnusedMethod", "UnusedVariable"})
@@ -27,8 +27,9 @@ public abstract class PlayerMixin extends LivingEntity {
         return flying || this.isSpectator();
     }
 
-    @Redirect(method = "hurtCurrentlyUsedShield", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;playSound(Lnet/minecraft/sounds/SoundEvent;FF)V"))
-    private void playShieldBreakSound(Player instance, SoundEvent soundEvent, float f, float g) {
+
+    @Inject(method = "disableShield", at = @At("TAIL"))
+    private void playShieldBreakSound(float f, CallbackInfoReturnable<Boolean> cir) {
         this.level.playSound(null, new BlockPos(this.position()), SoundEvents.SHIELD_BREAK, SoundSource.PLAYERS, 0.8F, 0.8F + this.level.random.nextFloat() * 0.4F);
     }
 }
